9961b392fa427592a374a879592889cebfc1278a
Ismael Meza Rodríguez <47857223+MezaDios@users.noreply.github.com>
2021-01-15 18:41:16 -0600

    Servidor terminado
    Se añadieron todas las rutas, las conexiones a la DB, los controladores asi como las llaves de acceso.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,39 @@
-console.log("works")
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import path from 'path';
+
+import userRoutes from './routes/userRoutes';
+
+class Server {
+
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    private config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    routes(): void {
+        this.app.use('/api/users', userRoutes);
+    }
+
+    start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log(`Server listening on port ${this.app.get('port')}`);
+        });
+    }
+}
+
+const server = new Server();
+server.start();
---
+++ server/src/keys.ts
@@ -0,0 +1,9 @@
+export default {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'ng_control',
+        port: 3306
+    }
+};
